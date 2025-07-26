73eaa432e06a483d750c4d5d04823d82ab79226b
akbar-ak-code <khanaliakbar7860@gmail.com>
2025-07-26 19:50:56 +0530

    II

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 export default function Navbar() {
