9943f9958200dea85d90c020d56092951f9acdce
akbar-ak-code <khanaliakbar7860@gmail.com>
2025-08-10 12:49:17 +0530

    IX

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/movie-explorer/">
+    <BrowserRouter basename="/movie-explorer">
       <App />
     </BrowserRouter>
   </StrictMode>
