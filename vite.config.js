687297b17029f13275eaacc3e0e0cf3b8f171a20
akbar-ak-code <khanaliakbar7860@gmail.com>
2026-04-11 19:15:46 +0530

    new fixes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/movie-explorer/',
+  // Change this from '/movie-explorer/' to '/'
+  base: '/', 
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // ← allows external access
-    port: 5173       // optional, default is 5173
-  }
-});
+})
